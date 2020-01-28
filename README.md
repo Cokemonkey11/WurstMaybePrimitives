79c19d00beb733c44574fb3fc702b0295daef24d
Cokemonkey11 <Cokemonkey11@gmail.com>
2020-01-28 21:26:56 +0000

    implement get_or_else and flat_map; resolve some bugs

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
-# How to render:
+Maybe (aka Option) types for wurst, using tuples for storage.
 
+Features:
+
+- support for `vec2`, `vec3`, and `angle` (as well as the expected primitives)
+- `map`/`flat_map` between supported types
+- constructors that infer presence based on nullity
+
+Example:
+
+```wurst
+import Abilities
+import ClosureTimers
+import GameTimer
+
+import WurstMaybePrimitives
+
+
+constant DUMMY = createUnit(players[0], 'hfoo', ZERO2, angle(0.))
+
+/**
+    Asynchronous.  Adds an effect on the provided unit, in its facing direction.
+    If the unit is `null`, then a global dummy is instead utilised, and the
+    angle is chosen randomly. If the provided effect is null, holy bolt is used.
+*/
+function expressiveEffect(unit onWho, string fx)
+    let u = maybe_unit(onWho).or_else(DUMMY)
+    let ang = maybe_unit(onWho)
+        .map(u -> u.getFacingAngle())
+        .get_or_else(() -> angle(getElapsedGameTime()))
+    let path = maybe_string(fx).get_or(Abilities.holyBoltMissile)
+
+    doPeriodicallyCounted(ANIMATION_PERIOD, 30) _cb ->
+        flashEffect(path, u.getPos().polarOffset(64., ang))
+```
+
+# Contributing - how to render:
+
+```bash
 ./render.sh
-
-# TODO
-
-- [ ] `map<T, U>`
-- [ ] `flat_map`
+```
--- _gen/src/main.rs
+++ _gen/src/main.rs
@@ -4,7 +4,6 @@ use std::io::{Result, Write};
 
 use askama::Template;
 
-
 struct TypeAndDefault<'a> {
     _type: &'a str,
     default: &'a str,
@@ -16,25 +15,56 @@ struct WurstMaybePrimitives<'a> {
     type_and_defaults: &'a [TypeAndDefault<'a>],
 }
 
-
 fn main() -> Result<()> {
-    let mut out_file = File::create(args().nth(1).unwrap_or("out.wurst".into()))?;
+    let mut out_file = File::create(args().nth(1).unwrap_or_else(|| "out.wurst".into()))?;
 
     let template = &WurstMaybePrimitives {
         type_and_defaults: &[
-            TypeAndDefault { _type: "int",    default: "0" },
-            TypeAndDefault { _type: "real",   default: "0." },
-            TypeAndDefault { _type: "bool",   default: "false" },
-            TypeAndDefault { _type: "string", default: "\"\"" },
-            TypeAndDefault { _type: "unit",   default: "null" },
-            TypeAndDefault { _type: "player", default: "null" },
-            TypeAndDefault { _type: "vec2",   default: "ZERO2" },
-            TypeAndDefault { _type: "vec3",   default: "ZERO3" },
-            TypeAndDefault { _type: "angle",  default: "angle(0.)" },
+            TypeAndDefault {
+                _type: "int",
+                default: "0",
+            },
+            TypeAndDefault {
+                _type: "real",
+                default: "0.",
+            },
+            TypeAndDefault {
+                _type: "bool",
+                default: "false",
+            },
+            TypeAndDefault {
+                _type: "string",
+                default: "null",
+            },
+            TypeAndDefault {
+                _type: "unit",
+                default: "null",
+            },
+            TypeAndDefault {
+                _type: "player",
+                default: "null",
+            },
+            TypeAndDefault {
+                _type: "vec2",
+                default: "ZERO2",
+            },
+            TypeAndDefault {
+                _type: "vec3",
+                default: "ZERO3",
+            },
+            TypeAndDefault {
+                _type: "angle",
+                default: "angle(0.)",
+            },
         ][..],
     };
 
-    out_file.write_all(template.render().expect("Failed to render template").as_bytes())?;
+    out_file.write_all(
+        template
+            .render()
+            .expect("Failed to render template")
+            .as_bytes(),
+    )?;
 
     Ok(())
 }
